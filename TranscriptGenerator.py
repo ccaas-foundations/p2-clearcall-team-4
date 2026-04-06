aef8966d3302f05790fa441c5e8870cc9208c18c
Tristan-Ballin <tballin2000@hotmail.com>
2026-04-06 14:21:50 -0400

    added more logic to generator and moved gitignore

--- TranscriptGenerator.py
+++ TranscriptGenerator.py
@@ -1,10 +1,42 @@
 import random
+import uuid
+from datetime import datetime, timezone, timedelta
 
-counter = 2
-while counter < 1:
-    generatedId = random.randint(0,100)
+counter = 0
+while counter < 5:
+    generatedId = uuid.uuid4()
+    startTime = datetime.now(tz=timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0)
+    random_seconds = random.randint(0, 86399)
+    startTime = startTime + timedelta(seconds=random_seconds)
+    random_seconds = random.randint(120, 300)
+    endTime = startTime + timedelta(seconds=random_seconds)
+    startTime = startTime.strftime('%Y-%m-%dT%H:%M:%SZ')
+    endTime = endTime.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    callCategory = random.randint(1,100)
+    ivrContainment = random.randint(1,100)
+    if callCategory>90:
+        callCategory = "GENERAL"
+    elif callCategory>75:
+        callCategory = "SALES"
+    elif callCategory>40:
+        callCategory = "TECHNICAL"
+    else:
+        callCategory = "BILLING"
+
+    ivrContainment = random.randint(1,100)
+    if callCategory == "SALES" or callCategory =="TECHNICAL":
+        ivrContainment="false"
+        escalatedToAgent = "true"
+    elif ivrContainment<61:
+        ivrContainment = "true"
+        escalatedToAgent = "false"
+    else:
+        ivrContainment = "false"
+        escalatedToAgent = "true"
+
     print(generatedId)
     scriptFile =open(f"./transcripts/{generatedId}.txt",'w')
-    scriptFile.write("{\n    \"callId\": \"a3f9c2e1-7b44-4d18-9f2c-1a2b3c4d5e6f\",\n    \"startTime\": \"2026-03-17T09:42:00Z\",\n    \"endTime\": \"2026-03-17T09:44:35Z\",\n    \"callCategory\": \"BILLING\",\n    \"ivrContained\": true,\n    \"escalatedToAgent\": false,\n    \"agentId\": null,\n    \"ivrPath\": [\"WELCOME\", \"BILLING\",\"ACCOUNT_LOOKUP\", \"RESOLVED\"]\n}")
+    scriptFile.write(f"\n    \"callId\": \"{generatedId}\",\n    \"startTime\": \"{startTime}\",\n    \"endTime\": \"2026-03-17T09:44:35Z\",\n    \"callCategory\": \"{callCategory}\",\n    \"ivrContained\": {ivrContainment},\n    \"escalatedToAgent\": {escalatedToAgent},\n    \"agentId\": null,\n    \"ivrPath\": [\"WELCOME\", \"BILLING\",\"ACCOUNT_LOOKUP\", \"RESOLVED\"]\n")
     scriptFile.close()
     counter+=1
