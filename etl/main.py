ca5a6f9d97c1a07fd0e210b8fcbebe94e55151ca
Tristan-Ballin <tballin2000@hotmail.com>
2026-04-15 19:33:14 -0400

    added argparse to main to allow cli running

--- etl/main.py
+++ etl/main.py
@@ -1,11 +1,47 @@
 from parser import TranscriptParser
 from transformer import TranscriptTransformer
+from loader import CassandraLoader
+import argparse
 
-parser = TranscriptParser("../transcripts")
-transformer = TranscriptTransformer()
 
-transcripts = parser.validate_transcripts()
+def parse_args():
+    parser = argparse.ArgumentParser(description="Generate random call transcript JSON files.")
+    parser.add_argument("-i",
+                        "--input",
+                        type=str,
+                        default="../transcripts",
+                        help="Input directory for transcript files (default: transcripts)"
+                        )
+    parser.add_argument("-H",
+                        "--host",
+                        type=str,
+                        default="localhost",
+                        help="Host for Cassandra Cluster (default: localhost)",
+                        )
+    parser.add_argument("-k",
+                        "--keyspace",
+                        type=str,
+                        default="clearcall",
+                        help="Keyspace in Cassandra Database (default: clearcall)",
+                        )
+    return parser.parse_args()
 
-for transcript in transcripts:
-    record = transformer.transform_transcript(transcript)
-    print(record)
+def main():
+    args = parse_args()
+
+    parser = TranscriptParser(args.input)
+    transformer = TranscriptTransformer()
+    loader = CassandraLoader(args.host,args.keyspace)
+
+    transcripts = parser.validate_transcripts()
+    loader.connect()
+
+    for transcript in transcripts:
+        record = transformer.transform_transcript(transcript)
+        loader.load(record)
+        print(record)
+
+    loader.close()
+
+if __name__ == "__main__":
+    main()
